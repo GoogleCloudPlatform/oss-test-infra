fd4251a0393083e21d9259f849327af9ef57eca1
Cole Wagner <colew@google.com>
2021-11-03 17:57:41 -0700

    Add alerting config for istio-testing.

--- prow/oss/terraform/main.tf
+++ prow/oss/terraform/main.tf
@@ -80,6 +80,16 @@ module "alert" {
       sinker : { namespace : "default" }
       tide : { namespace : "default" }
     }
+    istio-testing = {
+      crier : { namespace : "default" }
+      deck : { namespace : "default" }
+      ghproxy : { namespace : "default" }
+      hook : { namespace : "default" }
+      horologium : { namespace : "default" }
+      prow-controller-manager : { namespace : "default" }
+      sinker : { namespace : "default" }
+      tide : { namespace : "default" }
+    }
   }
   // blackbox_probers maps HTTPS hosts to the project they should be associated with.
   blackbox_probers = [
@@ -91,11 +101,14 @@ module "alert" {
     "gubernator.k8s.io",
     // knative-prow
     "https://prow.knative.dev",
+    // istio-testing
+    "https://prow.istio.io",
   ]
 
   bot_token_hashes = [
     "5514c8081c74362c58993e5de935cb92e38cc9397e57a72883c1878cfcdd4b38", // google-oss-robot
     "c7abc22fe95b5e3f849ab9fe9cd4809b5a1b950b44d0ea2bff1a5f1402d92b60", // knative-prow-robot
+    "89baaef92d6c5da4c2261911d273d4a01ef27d80f2bb7d517185f6f0416be8b5", // istio-testing
     // Ignore k8s-ci-robot until we resolve the token remaining inaccuracies.
     // "6624f39f2213835d6c820aff41666853557f99155d23cc52cd9171bcbed3dccc", // k8s-ci-robot
   ]
@@ -103,5 +116,6 @@ module "alert" {
     "oss-prow" = 15
     "k8s-prow" = 10
     "knative-prow" = 60
+    "istio-testing" = 30
   }
 }
